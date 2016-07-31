64ec0bff4afa0eb8b29870a0aa1d447c8e3f8cf8
Tom Smyth <tom@sassafras.coop>
2016-07-31 16:02:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redmine Public Wiki Pages Plugin
+
+Allows you to mark individual wiki pages as public, requiring no login.
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :public_wiki_pages do
+  name 'Public Wiki Pages plugin'
+  author 'Tom Smyth'
+  description 'Allows marking individual wiki pages to be publicly accessible via URL'
+  version '0.0.1'
+  url 'https://github.com/sassafrastech/public-wiki-pages'
+  author_url 'http://sassafras.coop'
+end
