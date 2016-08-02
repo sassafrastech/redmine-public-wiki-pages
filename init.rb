65393fa7ccc8547e01788a67beb76ef67ebd3b0c
Tom Smyth <tom@sassafras.coop>
2016-08-02 13:00:46 -0400

    Fixed plugin URL

--- init.rb
+++ init.rb
@@ -17,6 +17,6 @@ Redmine::Plugin.register :public_wiki_pages do
   author 'Tom Smyth'
   description 'Allows marking individual wiki pages to be publicly accessible via URL'
   version '0.0.1'
-  url 'https://github.com/sassafrastech/public-wiki-pages'
+  url 'https://github.com/sassafrastech/redmine-public-wiki-pages'
   author_url 'http://sassafras.coop'
 end
